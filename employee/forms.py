a07ea5e1eebd1e274ab8aa9a27d9f6be9a0e8b6a
Akash NP <akashnp1998@gmail.com>
2022-11-11 05:25:13 +0530

    Create Forms for CustomUser creation & updation
    Extending the UserCreationForm & UserChangeForm, this will allow user creation
    based on our CustomUser fields on both admin as well as template.

---
+++ employee/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
+from .models import CustomUser, Employee
+
+class SignUpForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+class UserUpdateForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
