be3fe539f1b79c6b505d2e2cf76b212425823162
Akash NP <akashnp1998@gmail.com>
2022-11-11 06:03:32 +0530

    Add Custom Login & Logout CBV
    - This allows email based login & logout extending django auth views
    - Overiding the next_page attribute to redirect to home & login page
    respectively.

--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+
+class CustomLoginView(LoginView):
+    next_page = 'home'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
