f1f3d1d299076c85b0e77a29349ee1ff05c2cfab
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-11-11 07:03:01 +0530

    Add Login Functionality (#1)
    * Add Custom Login & Logout CBV
    
    - This allows email based login & logout extending django auth views
    - Overiding the next_page attribute to redirect to home & login page
    respectively.
    
    * Setup URLs for Authentication using Django Authentication
    
    - setup login & logout urls pointing to the custom authentication views
    - include django auth urls for other auth functions (change password etc)
    
    * Add Custom Login template for Login View

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from employee.views import CustomLoginView, CustomLogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/logout/', CustomLogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('employee.urls')),
 ]
