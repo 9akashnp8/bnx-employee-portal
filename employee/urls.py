3fb28e82da22173999d0154e870e321d3f9f0582
Akash NP <akashnp1998@gmail.com>
2022-11-12 23:00:18 +0530

    Fix duplicate named urls

--- employee/urls.py
+++ employee/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('employee/add/', views.EmployeeCreateView.as_view(), name='add_employee'),
-    path('employee/add/salary/', views.EmployeeSalaryUpdateView.as_view(), name='update_employee'),
+    path('employee/add/salary/', views.EmployeeSalaryUpdateView.as_view(), name='update_employee_salary'),
     path('employees/', views.EmployeeListView.as_view(), name='list_employee'),
     path('employee/<int:pk>', views.EmployeeDetailView.as_view(), name='detail_employee'),
     path('employee/<int:pk>/update/', views.EmployeeUpdateView.as_view(), name='update_employee'),
